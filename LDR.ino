18924965f22f9850f8a382c6389bf6ae95d33582
shifa309 <shifaimran309@gmail.com>
2022-05-15 10:57:29 +0500

    LDR chnage

--- LDR.ino
+++ LDR.ino
@@ -1,12 +1,23 @@
 #define Light 27  
 
  void setup() {  
- Serial.begin(115200);  
+ Serial.begin(115200); 
+ pinMode (26, OUTPUT); 
  delay(1000);  
  }  
 
  void loop() {  
- int val = analogRead(Light);  
+ int val = analogRead(Light);
+
+   if (val<1800)
+   {
+    digitalWrite (26, HIGH);
+   }
+  else
+  {
+    digitalWrite (26, LOW);
+  }
+  
  Serial.println(val);  
  delay(1000);  
  }  
